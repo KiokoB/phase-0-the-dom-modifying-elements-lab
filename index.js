8a81ebb9b7d96d008019d261341c08ff39978684
Bianca Kioko <kiokobianca01@gmail.com>
2024-04-05 18:19:59 +0300

    removed id,created h1-element,added an id and text

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code here!
+const main = document.querySelector("#main")
+//console.log(main)
+main.remove("main");
+const newHeader = document.createElement("h1");
+//newHeader.nodeName = h1;
+//document.body.append(newHeader)
+// console.log(newHeader)
+newHeader.id = "victory"
+// console.log(newHeader)
+newHeader.innerHTML = "Bianca Kioko is the champion"
+console.log(newHeader.innerHTML)
